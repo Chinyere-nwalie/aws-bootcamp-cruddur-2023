e11d867929be89a8f97e8b8366437ea2da180e97
Nwalie Chinyere <nwaliechinyerejessica@gmail.com>
2023-03-24 13:13:09 +0000

    Fixed  post confirmation bugs

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,14 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 DROP TABLE IF EXISTS public.users;
 DROP TABLE IF EXISTS public.activities;
-
-
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  display_name text NOT NULL,
-  handle text NOT NULL,
-  email text NOT NULL,
-  cognito_user_id text NOT NULL,
+  display_name text,
+  handle text,
+  cognito_user_id text,
   created_at TIMESTAMP default current_timestamp NOT NULL
 );
 
