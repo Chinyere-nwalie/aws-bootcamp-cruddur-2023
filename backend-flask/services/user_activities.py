995648a35ff4596a17d8f6ac8464015a8fdadce7
Nwalie Chinyere <nwaliechinyerejessica@gmail.com>
2023-05-24 17:54:43 +0000

    Configuring for container insights

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
     # x-ray ---
