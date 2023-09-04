955f03ca349daa848d71b89f3ce81c7f3110ef46
Nwalie Chinyere <nwaliechinyerejessica@gmail.com>
2023-09-04 16:16:04 +0000

    Working on implementing migrations, implement the profile page image, and the endpoint of update profile

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -41,8 +41,10 @@ class Db:
     print("\n")
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
     print(sql,params)
-  def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning',sql,params)
+
+  def query_commit(self,sql,params={},verbose=True):
+    if verbose:
+      self.print_sql('commit with returning',sql,params)
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
@@ -60,8 +62,9 @@ class Db:
       self.print_sql_err(err)
   
   # when we want to return a json object
-  def query_array_json(self,sql,params={}):
-    self.print_sql('array',sql,params)
+  def query_array_json(self,sql,params={},verbose=True):
+    if verbose:
+      self.print_sql('array',sql,params)
     
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
@@ -70,10 +73,11 @@ class Db:
         json = cur.fetchone()
         return json[0]
   # when we want to return an array of json objects
-  def query_object_json(self,sql,params={}):
+  def query_object_json(self,sql,params={},verbose=True):
+    if verbose:
+      self.print_sql('json',sql,params)
+      self.print_params(params)
 
-    self.print_sql('json',sql,params)
-    self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
 
     with self.pool.connection() as conn:
@@ -85,8 +89,9 @@ class Db:
         else:
           return json[0]
   
-  def query_value(self,sql,params={}):
-    self.print_sql('value',sql,params)
+  def query_value(self,sql,params={},verbose=True):
+    if verbose:
+      self.print_sql('value',sql,params)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(sql,params)
